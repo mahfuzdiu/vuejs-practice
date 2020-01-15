4ca5c1c7818e2afb81ae97dc3d515163d289fdc5
Mahfuz18 <mahfuzur.rahman@webable.com.bd>
2020-01-15 13:31:14 +0600

    vue js add, edit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,84 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="col-md-12">
+    
+      <div class="row mt-3" v-for="audience in audiences" :key="audience.id">
+        <div class="col-sm-3">{{audience.id}}</div>
+        <div class="col-sm-3">{{audience.name}}</div>
+        <div class="col-sm-3">{{audience.location}}</div>
+        <div class="col-sm-3">
+          <button @click = "editAudience(audience)" class="btn btn-primary ml-2">Edit</button>
+          
+        </div>
+        
+      </div>
+
+      <div class="mt-3">
+        <form class="form-group col-sm-8 offset-sm-2">
+          <input v-model = "audience.id" type="text" class="form-control" placeholder="">
+          <input v-model = "audience.name" type="text" class="form-control mt-2" >
+          <input v-model = "audience.location" type="text" class="form-control mt-2" >
+        </form>
+      </div>
+
+      <div class="mt-4">
+        <button @click = "addAudience" class="btn btn-primary ">Add Audience</button>      
+      </div>
+      
+    
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  
+  data(){
+    return{
+      audiences : [
+        {
+          "id": 1,
+          "name" : "karim",
+          "location" : "dhanmondi"
+        },
+
+        {
+          "id": 2,
+          "name" : "rahim",
+          "location" : "dhanmondi"
+        },
+
+        {
+          "id": 3,
+          "name" : "solim",
+          "location" : "dhanmondi"
+        },
+
+        {
+          "id": 4,
+          "name" : "rafik",
+          "location" : "dhanmondi"
+        }
+      ],
+
+      // person: {} gives reference problem. so it needs destructurig to solve
+      // or we can use array
+      audience : []
+    }
+  },
+
+  methods: {
+    addAudience(){
+      this.audiences.push(this.audience)
+      this.audience = []
+    },
+
+    editAudience(audience){
+      this.audience = audience
+    }
   }
+
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
